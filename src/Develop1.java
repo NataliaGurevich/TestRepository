ce053296c70971f0881e1bc05ff1509a738072a2
NataliaGurevich <gurevich.natalia@gmail.com>
2019-10-06 22:46:59 +0200

    5 commit for dev2

--- src/Develop1.java
+++ src/Develop1.java
@@ -1,7 +1,10 @@
 public class Develop1 {
+
   String str = "Hallo";
   int i = 1;
   long l = 1L;
 
-  System.out.println("Hello world!");
+  public void world() {
+    System.out.println("Hello world!");
+  }
 }
